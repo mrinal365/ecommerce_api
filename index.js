caeb5fd87bcc5b2aa57305b7a82dcebf3c199720
Mrinal <mrinaltewary@gmail.com>
2023-06-21 19:44:52 +0530

    database connection

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const moongoose = require('mongoose');
+const dotenv = require('dotenv')
 
 const app = express();
+dotenv.config()
+app.use(express.json())
 
-app.listen('8001',()=>{
+moongoose.connect(process.env.MONGO_URL)
+    .then((res) => {
+        console.log("DB Connected")
+    })
+    .catch((err) => { console.log(err.message) })
+
+app.listen(process.env.PORT|| 8001, () => {
     console.log("Server is connected")
 })
+
+
+// mongodb+srv://mrinaltewary:<password>@whalehub.nhfqmla.mongodb.net/?retryWrites=true&w=majority
+
