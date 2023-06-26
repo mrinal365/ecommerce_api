24b3560484d59c1cb19cfe05a8c01fcd1314aaba
Mrinal <mrinaltewary@gmail.com>
2023-06-26 23:55:53 +0530

    authorisation initial

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,24 @@
-const { verifyToken } = require('./verifyToken');
-
+const User = require('../models/User');
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
 const router = require('express').Router();
 
-router.put('/:id', verifyToken ,(req,res)=>{
-    res.send("user route")
+router.put('/:id', verifyTokenAndAuthorization , async(req,res)=>{
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString()
+    }
+
+    try{
+        const updatedUser = User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        },{new:true})
+        
+        res.status(200).json(updatedUser)
+    } catch(err){
+        res.status(500).json(err)
+    }
 });
   
 router.post('/',(req,res)=>{
