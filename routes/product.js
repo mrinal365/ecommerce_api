2d7a77ff27612685b852157e434bfe86a400699e
Mrinal <mrinaltewary@gmail.com>
2023-07-20 23:57:24 +0530

    product

--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,113 @@
 const router = require('express').Router();
+const Product = require('../models/Product');
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
+
+//CREATE
+router.post('/')
 
 
-module.exports= router
+// UPDATE
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString()
+    }
+    try {
+        console.log("req", req.params.id)
+        console.log("req", req.body)
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        // console.log("vgjhgv", updatedUser)
+        res.status(200).json(updatedUser)
+    } catch (err) {
+        // console.log("gfh",err.message)
+        res.status(500).json(err)
+    }
+});
+
+
+// DELETE
+router.delete('/:id', verifyTokenAndAuthorization, async(req,res)=>{
+    try{
+        await User.findByIdAndDelete(
+            req.params.id,
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json("User Has been deleted");
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//GET USER
+router.get('/find/:id', verifyTokenAndAdmin, async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+// GET ALL USERS
+router.get('/users', verifyTokenAndAdmin, async(req,res)=>{
+    const query = req.query.new;
+    console.log("query",query)
+    try{
+        const users = query 
+            ? await User.find().sort({ _id: -1 }).limit(1)
+            : await User.find();
+            // const { password, ...others } = user._doc;
+        res.status(200).json(users);
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+// GET USER STATS
+router.get('/stats',verifyTokenAndAuthorization,async (req, res)=>{
+    const date = new Date();
+    //get last year date
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+    console.log("hwuighwd")
+    try{
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear}} },
+            {
+                $project: {
+                    month: {
+                        $month: "$createdAt"
+                    },
+                }
+            },
+            {
+                $group:{
+                    _id: "$month",
+                    total: {$sum: 1},
+                }
+            }
+        ])
+        res.status(200).json(data)
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+router.post('/', (req, res) => {
+    const input = req.body.input;
+    console.log("hey")
+    res.json(input)
+})
+
+module.exports = router
+
--- routes/user.js
+++ routes/user.js
@@ -56,15 +56,48 @@ router.get('/find/:id', verifyTokenAndAdmin, async(req,res)=>{
 
 // GET ALL USERS
 router.get('/users', verifyTokenAndAdmin, async(req,res)=>{
+    const query = req.query.new;
+    console.log("query",query)
     try{
-        const users = await User.find();
-        // const { password, ...others } = user._doc;
+        const users = query 
+            ? await User.find().sort({ _id: -1 }).limit(1)
+            : await User.find();
+            // const { password, ...others } = user._doc;
         res.status(200).json(users);
     } catch(err){
         res.status(500).json(err)
     }
 })
 
+// GET USER STATS
+router.get('/stats',verifyTokenAndAuthorization,async (req, res)=>{
+    const date = new Date();
+    //get last year date
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+    console.log("hwuighwd")
+    try{
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear}} },
+            {
+                $project: {
+                    month: {
+                        $month: "$createdAt"
+                    },
+                }
+            },
+            {
+                $group:{
+                    _id: "$month",
+                    total: {$sum: 1},
+                }
+            }
+        ])
+        res.status(200).json(data)
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
 
 router.post('/', (req, res) => {
     const input = req.body.input;
